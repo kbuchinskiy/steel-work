28b0f1a3f37dc67224a6d66fc905f05b41c84a10
k.buchinskiy <k.buchinskiy@viseven.com>
2019-04-01 17:02:32 +0300

    update(prod)

---
+++ prod/js/product.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    const illustrationBlock = document.querySelector('#technology1 .illustration-block');
+
+    illustrationBlock.addEventListener('click', toggleCaption);
+
+    function toggleCaption(e) {
+        if (e.target.classList.contains('toggle-caption-btn')) {
+            e.target.closest('.caption-container').classList.toggle('active');
+        }
+    }
+});
